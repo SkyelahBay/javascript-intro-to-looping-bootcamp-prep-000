0baf493fe615cc1f50e7acbd65066a8d76f7f78a
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-12 21:27:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 function forLoop(array){
+  let arrayName = array;
   for(var i = 0; i < 25; i++){
-    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    arrayName.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
 }
 function whileLoop(n){
